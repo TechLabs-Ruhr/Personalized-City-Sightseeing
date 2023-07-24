0937719ee47d1187e5773eb6e2bd0d7ababec6f9
Daniel <daniel.beck006@stud.fh-dortmund.de>
2023-07-24 15:00:31 +0200

    userAnswers in array

--- userPrompt.py
+++ userPrompt.py
@@ -12,31 +12,37 @@ def ask_question_with_choices(question, choices):
     except ValueError:
         pass
 
+    # Falls keine gültige Nummer eingegeben wurde oder Freitext gewünscht ist
     return choice_number
 
 def main():
     print("Willkommen! Beantworten Sie bitte einige Fragen, um den idealen Urlaub für Sie zu planen.")
 
+    user_answers = {}
+
     interests_choices = ["Natur", "Kultur", "Geschichte", "Kunst", "Musik", "Essen", "Sport", "Entspannung"]
-    interests = ask_question_with_choices("Was sind Ihre Interessen?", interests_choices)
+    user_answers["Interessen"] = ask_question_with_choices("Was sind Ihre Interessen?", interests_choices)
 
     vacation_type_choices = ["Aktivurlaube", "Entspannende Urlaube"]
-    vacation_type = ask_question_with_choices("Mögen Sie Aktivurlaube oder bevorzugen Sie eher entspannende Urlaube?", vacation_type_choices)
+    user_answers["Urlaubstyp"] = ask_question_with_choices("Mögen Sie Aktivurlaube oder bevorzugen Sie eher entspannende Urlaube?", vacation_type_choices)
 
     climate_preference_choices = ["Wärmer", "Kühler", "Gemäßigt"]
-    climate_preference = ask_question_with_choices("Welche Klimazone bevorzugen Sie?", climate_preference_choices)
+    user_answers["Klimazone"] = ask_question_with_choices("Welche Klimazone bevorzugen Sie?", climate_preference_choices)
 
     continent_choices = ["Europa", "Amerika", "Asien", "Afrika", "Australien"]
-    continent_preference = ask_question_with_choices("Gibt es Kontinente die Sie besonders interessieren?", continent_choices)
+    user_answers["Kontinente"] = ask_question_with_choices("Gibt es Kontinente die Sie besonders interessieren?", continent_choices)
 
     city_or_rural_choices = ["Stadt", "Ländlich"]
-    city_or_rural = ask_question_with_choices("Städtische oder Ländliche Gebiete?", city_or_rural_choices)
+    user_answers["Stadt oder Land"] = ask_question_with_choices("Städtische oder Ländliche Gebiete?", city_or_rural_choices)
 
     travel_type_choices = ["Alleine", "mit einem Partner", "in einer Gruppe"]
-    travel_type = ask_question_with_choices("Reisen Sie alleine, mit einem Partner, mit der Familie oder in einer Gruppe?", travel_type_choices)
+    user_answers["Alleine oder in einer Gruppe"] = ask_question_with_choices("Reisen Sie alleine, mit einem Partner, mit der Familie oder in einer Gruppe?", travel_type_choices)
+    # Weitere Fragen und Antwortmöglichkeiten hier ergänzen...
 
-
-    print("\nVielen Dank für Ihre Antworten! Basierend auf Ihren Angaben werden wir den idealen Urlaub für Sie planen.", "freitext")
+    print("\nVielen Dank für Ihre Antworten! Basierend auf Ihren Angaben werden wir den idealen Urlaub für Sie planen.")
+    print("Ihre Antworten:")
+    for question, answer in user_answers.items():
+        print(f"{question}: {answer}")
 
 if __name__ == "__main__":
     main()
