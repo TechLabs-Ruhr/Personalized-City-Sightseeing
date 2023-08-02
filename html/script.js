f7bee55a1508a276b4b99caa2210e5f48534b9b5
Sally-joo <sally.hofacker@rub.de>
2023-08-02 17:52:45 +0200

    checkpoint

---
+++ html/script.js
@@ -0,0 +1,12 @@
+function openModal() {
+    document.getElementById("registration-modal").style.display = "block";
+  }
+
+  function closeModal() {
+    document.getElementById("registration-modal").style.display = "none";
+  }
+  
+  document.getElementById("create-account").addEventListener("click", openModal);
+  
+  document.getElementById("close-modal").addEventListener("click", closeModal);
+  
